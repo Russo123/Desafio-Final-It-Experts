c226e70e9657da840f76528027c5dbdee89d0fa0
Fabiana Alves <fabi.pereiraa@hotmail.com>
2022-03-18 16:47:06 -0300

    Criando o Controller e Service

---
+++ src/main/java/com/itexperts/projeto/controller/AccountController.java
@@ -0,0 +1,36 @@
+package com.itexperts.projeto.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.itexperts.projeto.model.Account;
+import com.itexperts.projeto.service.AccountService;
+
+@RestController
+@RequestMapping("api/v1/account")
+public class AccountController {
+
+	@Autowired
+	private AccountService accountService;
+
+	@PostMapping
+	public ResponseEntity<Account> create(@RequestBody Account account) {
+		Account a = new Account();
+		a = accountService.create(account);
+
+		return ResponseEntity.ok().body(a);
+
+	}
+
+	public ResponseEntity<Account> getById(@PathVariable Long id) {
+		Account account = accountService.getById(id);
+		return ResponseEntity.ok().body(account);
+
+	}
+
+}
---
+++ src/main/java/com/itexperts/projeto/service/AccountService.java
@@ -0,0 +1,37 @@
+package com.itexperts.projeto.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.itexperts.projeto.model.Account;
+import com.itexperts.projeto.repository.AccountRepository;
+
+@Service
+public class AccountService {
+	
+	@Autowired
+	private AccountRepository accountRepository;
+	
+	public Account create(Account account) {
+		Account a = new Account();
+		a = accountRepository.save(account);
+		return a;
+	}
+
+	public Account getById(Long id) {
+		
+		return null;
+	}
+	
+//	public void update(Account account, Long id) {
+//		Optional<Account> accountReturned = accountRepository.findById(id);
+//		accountReturned.orElseThrow(() -> new RuntimeException("Account not found"));
+//		
+//		accountReturned.get().setNameOwner(account.getNameOwner());
+//		accountReturned.get().getAccountCode(account.get());
+//		accountReturned.save(accountReturned.get());
+//	}
+
+}
