39e4fdc2af98f9991bc05e61dda16eb46e4f2dab
Fabiana Alves <fabi.pereiraa@hotmail.com>
2022-03-26 22:17:04 -0300

    Atualizacao sawagger controller

--- src/main/java/com/itexperts/projeto/controller/AccountController.java
+++ src/main/java/com/itexperts/projeto/controller/AccountController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,14 +17,20 @@ import org.springframework.web.bind.annotation.RestController;
 import com.itexperts.projeto.model.Account;
 import com.itexperts.projeto.service.AccountService;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
 @RestController
 @RequestMapping("api/v1/account")
+@Api(value = "Api Rest Account")
+@CrossOrigin(origins = "*")
 public class AccountController {
 
 	@Autowired
 	private AccountService accountService;
 
 	@PostMapping
+	@ApiOperation(value = "Criação de contas")
 	public ResponseEntity<Account> create(@RequestBody Account account) {
 		Account a = new Account();
 		a = accountService.create(account);
